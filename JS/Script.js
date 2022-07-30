437781c1e815322e179a04713f672bb8522b31fb
Abhinav <75786072+ABHINAVstar05@users.noreply.github.com>
2022-07-30 22:45:23 +0530

    JavaScript File

---
+++ JS/Script.js
@@ -0,0 +1,8 @@
+document.getElementById("blink").style.fontWeight="900";
+var typed = new Typed('#blink', {
+  // Waits 1000ms after typing "First"
+  strings: ['', 'Coder .', 'Web Developer .', 'ML enthusiast .', 'Cyber enthusiast .'],
+  typeSpeed: 100,
+  backSpeed: 90,
+  loop:true
+});
