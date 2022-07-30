5304e6faa6b8466254aaa88eac7c1fa72e6b91e2
Abhinav <75786072+ABHINAVstar05@users.noreply.github.com>
2022-07-30 23:32:25 +0530

    JavaScript File

--- JS/Script.js
+++ JS/Script.js
@@ -1,7 +1,7 @@
 document.getElementById("blink").style.fontWeight="900";
 var typed = new Typed('#blink', {
   // Waits 1000ms after typing "First"
-  strings: ['', 'Coder .', 'Web Developer .', 'ML enthusiast .', 'Cyber enthusiast .'],
+  strings: ['', 'Coder .', 'Web Developer .', 'ML enthusiast .', 'Cyber security enthusiast .'],
   typeSpeed: 100,
   backSpeed: 90,
   loop:true
